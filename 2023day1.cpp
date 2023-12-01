e2801bbe85b9694799e2f1d3e9415ea916bdcf51
Aidar Tukmeev <aidar.tu.n@gmail.com>
2023-12-01 15:47:26 +0300

    day1

---
+++ 2023day1.cpp
@@ -0,0 +1,31 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <regex>
+
+using namespace std;
+
+int main() {
+	ifstream ifile("input1.txt", ios::in);
+	if (ifile.is_open()) {
+		string line;
+		int answer = 0;
+		while (getline(ifile, line)) {
+			stringstream ss(line);
+			auto res = cmatch{};
+			regex re(R"(\d)");
+			string first = "-1", last = "-1";
+			while (regex_search(line.c_str(), res, re)) {
+				if (first == "-1") {
+					first = *res[0].first;
+				}
+				last = *res[0].first;
+				line = res.suffix();
+			}
+			answer += stoi(first + last);			
+		}
+		cout << answer << endl;
+	}
+	return 0;
+}
